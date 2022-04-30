37ef9c9ca81191cd653337e3b7232a60b7b09046
HicaroD <hdanrlley1@gmail.com>
2022-04-30 17:56:36 -0300

    [LXR] Creating base Lexer struct

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,16 @@
 enum Token {
     Identifier(String),
+    Def,
+}
+
+pub struct Lexer {
+    source_code: String,
+}
+
+impl Lexer {
+    pub fn new(source_code: String) -> Self {
+        Self {
+            source_code
+        }
+    }
 }
