7c9dfd40637f105a6cfc55892a2bea0184a26942
HicaroD <hdanrlley1@gmail.com>
2022-05-21 19:58:29 -0300

    [PRS] building a couple AST Nodes

--- src/idk/parser.h
+++ src/idk/parser.h
@@ -5,9 +5,35 @@
 
 #include <vector>
 
-class Parser {
-    private:
-	std::vector<Token> tokens;
+enum class Type {
+    Int,
+    Float,
+    Boolean,
 };
+
+struct Parameter {
+    std::string name;
+    Type type;
+};
+
+struct ASTNode {};
+
+struct Variable : ASTNode {
+    std::string name;
+    Type type;
+
+    // It is not quite a string, it will be changed in the future
+    std::string value;
+};
+
+struct Function : ASTNode {
+    std::string return_type;
+    std::string name;
+    std::vector<Parameter> parameters;
+    // TODO: Declare the body of the function
+};
+
+// TODO: Declare parser functions (one for each non-terminal)
+class Parser {};
 
 #endif // PARSER_H 
