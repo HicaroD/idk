24a624ff44cafb596e491fd8e1b713888456299d
HicaroD <hdanrlley1@gmail.com>
2022-05-20 23:36:48 -0300

    [LXR] refactoring `EOF` checking

--- src/idk/lexer.h
+++ src/idk/lexer.h
@@ -8,7 +8,6 @@ class Lexer {
 	std::vector<char> source_code;
 	char current_char;
 	int position;
-	bool eof;
 
     public:
 	Lexer(const std::vector<char>& source);
@@ -16,7 +15,6 @@ class Lexer {
 	char get_current_char();
 	void advance();
 	bool is_eof();
-	void set_eof();
 };
 
 #endif
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -7,25 +7,18 @@ Lexer::Lexer(const std::vector<char>& source) {
     source_code = source;
     current_char = '0';
     position = 0;
-    eof = false;
 }
 
 void Lexer::advance() {
     if(source_code.empty()) {
 	exit(1);
-    } else if(position + 1 == int(source_code.size())) {
-	set_eof();
-    } else {
+    } else if(!is_eof()) {
 	current_char = source_code[position++];
     }
 }
 
 bool Lexer::is_eof() {
-    return eof;
-}
-
-void Lexer::set_eof() {
-    eof = true;
+    return position + 1 == int(source_code.size());
 }
 
 char Lexer::get_current_char() {
