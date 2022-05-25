e15acfb4ade3b4494a6d23aba5c60691fe4d8b5e
HicaroD <hdanrlley1@gmail.com>
2022-05-25 00:05:12 -0300

    [FMT] `clang-format --style Google`

--- src/idk/parser.h
+++ src/idk/parser.h
@@ -7,9 +7,9 @@
 #include <vector>
 
 enum class Type {
-    Int,
-    Float,
-    Boolean,
+  Int,
+  Float,
+  Boolean,
 };
 
 struct ASTNode {};
@@ -17,25 +17,25 @@ struct ASTNode {};
 struct Statement : ASTNode {};
 
 struct Variable : Statement {
-    Type type;
-    std::string name;
-    // It is not quite a string, it can be anything
-    std::string value;
+  Type type;
+  std::string name;
+  // It is not quite a string, it can be anything
+  std::string value;
 };
 
 Variable new_variable(Type type, std::string name, std::string value);
 
 class Parser {
-    private:
-	std::vector<Token> tokens;
-	std::vector<Token>::iterator cursor;
+ private:
+  std::vector<Token> tokens;
+  std::vector<Token>::iterator cursor;
 
-    public:
-	Parser(std::vector<Token> tokens_);
+ public:
+  Parser(std::vector<Token> tokens_);
 
-	Variable parse_variable_assignment();
+  Variable parse_variable_assignment();
 
-	void generate_ast();
+  void generate_ast();
 };
 
-#endif // PARSER_H 
+#endif  // PARSER_H
