21fc9c9429ce8aa7a628023d5d65781d4d2a62d5
HicaroD <hdanrlley1@gmail.com>
2022-07-16 17:37:01 -0300

    backend(c): generatinc c functions

--- src/backend/c.rs
+++ src/backend/c.rs
@@ -1,17 +1,28 @@
 use crate::ast::*;
+use crate::backend::evaluate_ast;
+use crate::backend::CodeGenerator;
 use std::fs::File;
 use std::io::Write;
 
 pub struct C {
-    ast: Vec<Ast>,
+    source_code: File,
+}
+
+impl CodeGenerator for C {
+    fn generate(&mut self, ast: Vec<Ast>) -> Result<(), String> {
+        self.generate_c_code(ast)
+    }
 }
 
 impl C {
-    pub fn new(ast: Vec<Ast>) -> Self {
-        Self { ast }
+    pub fn new() -> Self {
+        Self {
+            // FIXME: Avoid unwrap
+            source_code: File::create("code.c").unwrap(),
+        }
     }
 
-    pub fn get_c_type(&self, type_: &Type) -> Result<&str, String> {
+    fn get_c_type(&self, type_: &Type) -> Result<&str, String> {
         match type_ {
             Type::Int => Ok("int"),
             Type::Float => Ok("float"),
@@ -21,37 +32,62 @@ impl C {
         }
     }
 
-    pub fn get_function_parameters(&self, parameters: &Vec<Parameter>) -> Result<String, String> {
+    fn get_function_parameters(&self, parameters: &Vec<Parameter>) -> Result<String, String> {
         let mut c_parameters = String::new();
 
         for parameter in parameters {
             let parameter_type = self.get_c_type(&parameter.parameter_type)?;
-            c_parameters += &format!("{} {},", parameter_type, parameter.name);
+            c_parameters += &format!("{} {}, ", parameter_type, parameter.name);
         }
-
+        c_parameters = c_parameters.trim_end_matches(", ").to_string();
         Ok(c_parameters)
     }
 
-    pub fn build_c_function(&self, function_node: Function) -> Result<String, String> {
+    fn build_c_block(&self, block: Block) -> Result<String, String> {
+        let mut statements = String::new();
+
+        for statement in block.itens.iter() {
+            match statement {
+                Ast::Assignment(assignment) => {
+                    statements += &self.build_c_assignment(assignment.clone())?
+                }
+                _ => {
+                    return Err("Unexpected AST node".to_string());
+                }
+            };
+        }
+        Ok(statements)
+    }
+
+    pub fn build_c_assignment(&self, assignment: Assignment) -> Result<String, String> {
+        let var_type = self.get_c_type(&assignment.var_type)?;
+        let name = assignment.name;
+        let value = evaluate_ast(assignment.value)?.to_string();
+
+        Ok(format!("\t{} {} = {};\n", var_type, name, value))
+    }
+
+    fn build_c_function(&self, function_node: Function) -> Result<String, String> {
         let parameters = self.get_function_parameters(&function_node.parameters)?;
         let return_type = match function_node.return_type {
             Some(t) => self.get_c_type(&t)?,
             None => "void",
         };
+
+        let block = self.build_c_block(function_node.body)?;
+
         Ok(format!(
-            "{} {}({}) {{{}}}",
-            return_type, function_node.name, parameters, "statement here"
+            "{} {}({}) {{\n{}}}",
+            return_type, function_node.name, parameters, block
         ))
     }
 
-    pub fn generate_c_code(&self) -> Result<(), String> {
-        // TODO: Avoid unwrap
-        let mut c_code = File::create("code.c").unwrap();
-
-        for ast_node in self.ast.iter() {
-            match ast_node {
+    pub fn generate_c_code(&mut self, ast: Vec<Ast>) -> Result<(), String> {
+        for node in ast.iter() {
+            match node {
                 Ast::Function(function) => {
-                    c_code.write_all(self.build_c_function(function.clone())?.as_bytes());
+                    self.source_code
+                        .write_all(self.build_c_function(function.clone())?.as_bytes());
                 }
 
                 _ => return Err("Invalid AST node".to_string()),
